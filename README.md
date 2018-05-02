a7338c2c157f52a60b785a1c57dab0ba419d7d93
skylerreimer <skylerreimer@gmail.com>
2018-05-02 14:50:42 -0400

    make it better

---
+++ README.md
@@ -0,0 +1,65 @@
+# ASBot2.0
+Active development has discontinued. Feel free to contact me if you need help starting an instance of this bot on your own.
+
+# Features:
+
+%asbility: returns information about a given ability
+
+%asbitem: returns information about a given item
+
+%asbmove: returns information about a given move
+
+%asbnature: returns information about a given nature
+
+%asbstats: returns stats of a given pokemon
+
+%asbtype: returns information about a given type
+
+%calc: evaluates an expression
+
+%ping: responds pong, useful for checking if bot is alive
+
+%pong: responds pong, useful for checking if bot is alive
+
+%roll: gives a random number
+
+%help: gives a list of commands
+
+And much more! Try %help to get a full list of available commands
+
+# Installation:
+
+This bot is written to run on top of node.js. Please see https://nodejs.org/en/download/
+
+Once you have node installed running `npm install` from the bot directory should install all the needed packages. If an error happens something went wrong
+
+
+# Running:
+
+Before you run the bot you will need to download the latest spreadsheet. You can manually download it <a href="https://docs.google.com/spreadsheets/d/1RlsjFgpQGSZPGtwaUL33g0zHN888-iveQPKSVgFII-M/edit#gid=0">here</a> and then place it into the res folder as Data.xlsx.
+
+OR
+
+You can run the updater programs in the updater folder. downloadFile.js is a Node.js command line program that will use your google account to authorize the dowloading of the data.
+Then you can optionally run errorCheck.java to ensure the file you downloaded isn't corrupted.
+
+Anyways, you need to get the data for the bot into the res folder somehow. I personally use a cron job to redownload the data file every few days so my instance of the bot is updated for my users
+
+To start the bot just run
+`node app.js`.
+inside of src/bot. I personally use <a href="https://github.com/Unitech/pm2">pm2</a> to manage my bots. I use the same cron job from earlier to restart the bot and update it using the scripts I gave you in the scripts folder.
+
+
+# Updates: 
+
+If you update the bot, please run `npm update` before starting it again. If you have
+issues with this, you can try deleting your node_modules folder and then running
+`npm install` again. Please see [Installation](#Installation). 
+
+# Credits:
+
+The boys and girls at CAP ASB who have been using the bot and finding all the bugs for me
+
+My friend who initally asked if this project was possible @endless in the discord channel
+
+
---
+++ setup.js
@@ -0,0 +1,152 @@
+const config = require("./res/config.json");
+
+module.exports = {
+  getPokemonList: function(worksheet){
+    //add the pokemon to the map with their row number
+    var pokemonList = new Map();
+    var i = 2;
+    while(worksheet['A'+i].v != "HOUSE" ){
+      var pokemonName = worksheet['B'+i].v.toLowerCase();
+      pokemonName = pokemonName.replace(/\W/g, '');
+
+      //dealing with mega and primal pokemon
+      if(worksheet['A'+i].v == "Mega" || worksheet['A'+i].v == "Primal" || worksheet['A'+i].v == "Ultra"){
+        pokemonName = worksheet['A'+i].v.toLowerCase() + pokemonName;
+        console.log("added " + pokemonName);
+      }
+
+      pokemonList.set(pokemonName,i);
+      i++;
+
+    }
+
+    return pokemonList;
+  },
+
+  getNatureList: function(worksheet){
+    //someone decided to put spaces between groups of natures. if statement ignores those
+    var natureList = new Map();
+    for(i = 3; i <= config.numNatures; i++){
+      if(worksheet['A'+i] != undefined){
+        var cell = 'A'+i;
+        var natureCell = worksheet[cell];
+        var natureName = natureCell.v.toLowerCase();
+        natureName = natureName.replace(/\W/g, '');
+        natureList.set(natureName,i);
+      }
+    }
+    return natureList;
+  },
+
+  getAbilityList: function(worksheet){
+    //add of the abilities to the map with their row number
+    var abilityList = new Map();
+    var i = 2;
+    while(worksheet['A'+ i] != undefined){
+      var cell = 'A'+i;
+      var abilityCell = worksheet[cell];
+      var abilityName = abilityCell.v.toLowerCase();
+      abilityName = abilityName.replace(/\W/g, '');
+      abilityList.set(abilityName,i);
+      i++;
+    }
+
+    return abilityList;
+  },
+
+  getHeldItemList: function(worksheet){
+    var itemHeldList = new Map();
+
+    for(i = 2; i < config.numHeldItems; i++){
+      if(worksheet['A'+i] != undefined && worksheet['B'+i] != undefined){
+        var itemName = worksheet['A'+i].v.toLowerCase();
+        itemName = itemName.replace(/\W/g, '');
+        itemHeldList.set(itemName,i);
+      }
+    }
+    return itemHeldList;
+  },
+
+  getTlrItemList: function(worksheet){
+    var itemTLRList = new Map();
+    var i = 2;
+    while(worksheet['A'+i] != undefined){
+      var itemName = worksheet['A'+i].v.toLowerCase();
+      itemName = itemName.replace(/\W/g, '');
+      itemTLRList.set(itemName,i);
+      i++;
+    }
+    return itemTLRList;
+  },
+
+  getKeyItemList: function(worksheet){
+    var keyItemList = new Map();
+
+    for(i = 2; i <= config.numKeyItems; i++){
+      if(worksheet['A'+i] != undefined && worksheet['B'+i] != undefined){
+        var itemName = worksheet['A'+i].v.toLowerCase();
+        itemName = itemName.replace(/\W/g, '');
+        keyItemList.set(itemName,i);
+      }
+    }
+    return keyItemList;
+  },
+
+  getConsumableList: function(worksheet){
+    var consumableList = new Map();
+
+    var i = 2;
+    while(worksheet['A'+i] != undefined){
+      var itemName = worksheet['A'+i].v.toLowerCase();
+      itemName = itemName.replace(/\W/g, '');
+      consumableList.set(itemName,i);
+      i++;
+    }
+    return consumableList;
+  },
+
+  getTypeList: function(worksheet){
+    var typeList = new Map();
+
+    var i = 1;
+    while(worksheet['A'+i] != undefined){
+      var typeName = worksheet['A'+i].v.toLowerCase();
+      typeName.replace(/\W/g, '');
+      typeList.set(typeName,i);
+      i++;
+    }
+    return typeList;
+  },
+
+  getMoveList: function(worksheet){
+    var moveList = new Map();
+
+    //loop through all rows in the move worksheet
+    var i = 2;
+    var start = i;
+    var end = 0;
+
+    for(i = 2; i <= config.numMoves; i++){
+      if(worksheet['A' + i] != undefined){
+        end = i - 1;
+        var arr = [0,0];
+        arr[0] = start;
+        arr[1] = end;
+
+        var name = worksheet['A'+start].v.toLowerCase();
+        name = name.replace(" (move)","");
+        name = name.replace(" (command)","");
+        name = name.replace(/\W/g, '');
+
+        if(!name.includes("(users)") && name != "-"){
+          moveList.set(name , arr);
+        }
+
+        start = i;
+      }
+
+    }
+    return moveList;
+  }
+
+};
